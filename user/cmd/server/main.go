437ee253cec83e9502fb8bf0e279f5ca20a04efc
akaishi-hironori <akaishi-hironori@dmm.com>
2020-05-04 23:21:01 +0900

    Add a base server for the user service

--- user/cmd/server/main.go
+++ user/cmd/server/main.go
@@ -1,24 +1,48 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+
 	"google.golang.org/grpc"
-	pb "github.com/isy/grpc-sandbox/user/interface/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	pb "github.com/isy/grpc-sandbox/user/app/interface/grpc"
 )
 
-type Service struct {}
+type Service struct{}
 
 func main() {
+	lis, err := net.Listen("tcp", ":8080")
 	server := grpc.NewServer()
 
 	s := &Service{}
 
 	pb.RegisterUserServiceServer(server, s)
+
+	reflection.Register(server)
+
+	if err != nil {
+		fmt.Println("network I/O error: %v", err)
+		os.Exit(1)
+	}
+
+	if err := server.Serve(lis); err != nil {
+		fmt.Printf("serve error: %v", err)
+		os.Exit(1)
+	}
+	fmt.Println("Run server")
 }
 
-func (*Service) ListUsers(ctx context.Context) (*pb.ListUsersResponse, error) {
+func (s *Service) ListUsers(ctx context.Context, in *emptypb.Empty) (*pb.ListUsersResponse, error) {
 	fmt.Println("test")
 
-	return &pb.ListUsersResponse {
-
-	},
+	return &pb.ListUsersResponse{
+		Users: []*pb.User{
+			{Id: 1},
+		},
+	}, nil
 }
