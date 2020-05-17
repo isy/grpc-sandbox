61b676e140f337a5ff5467c0af8b00d50123aafb
akaishi-hironori <akaishi-hironori@dmm.com>
2020-05-18 01:01:20 +0900

    Adding functions to be executed in a transaction

---
+++ user/app/infra/db/mysql/tx.go
@@ -0,0 +1,30 @@
+package mysql
+
+import (
+	"log"
+
+	"github.com/jmoiron/sqlx"
+)
+
+func InTx(db *sqlx.DB, fn func(tx *sqlx.Tx) error) error {
+	tx, err := db.Beginx()
+	if err != nil {
+		return err
+	}
+
+	if err := fn(tx); err != nil {
+		if err := tx.Rollback(); err != nil {
+			log.Printf("Rollback error was occured: %+v", err)
+		}
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		if err := tx.Rollback(); err != nil {
+			log.Printf("Rollback error was occured: %+v", err)
+		}
+		return err
+	}
+
+	return nil
+}
